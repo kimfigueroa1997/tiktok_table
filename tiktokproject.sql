a31e9728c8f303907e516c9bb3521a00dd20dd39
kimfigueroa1997 <kimfigueroa1997@gmail.com>
2025-03-03 23:54:44 -0600

    Add files via upload

---
+++ tiktokproject.sql
@@ -0,0 +1,51 @@
+/* Outputs creators with follower count greater than 10,000 and gmv over $2000. */
+SELECT *
+FROM tiktok
+WHERE follower_count > 10000 AND
+	  gmv > 2000;
+      
+/* Outputs creators in order by follower count (highest to least). */
+SELECT creator, follower_count
+FROM tiktok
+ORDER BY follower_count DESC;
+
+/* Outputs creators with a follower count greater than 10,000 and gmv greater than $5,000 in 
+'beauty and personal care', 'health', 'food & beverage' and 'sports & outdoor' categories in order by gmv (highest to lowest). */
+SELECT *
+FROM tiktok 
+WHERE follower_count > 10000 AND
+	  gmv > 5000 AND 
+      gmv_category IN ('beauty & personal care', 'health', 'sports & outdoor', 'food & beverage')
+ORDER BY gmv DESC;
+
+/* Outputs creators in order by highest to lowest follower count*/
+SELECT creator, follower_count
+FROM tiktok
+ORDER BY follower_count DESC;
+
+/* Outputs creators with a follower count between 10,000 and 50,000 and gmv greater than $5,000 in 
+'beauty and personal care', 'health', 'food & beverage' and 'sports & outdoor' categories in order by gmv (highest to lowest). */
+SELECT *
+FROM tiktok 
+WHERE follower_count BETWEEN 10000 AND 50000 AND
+	  gmv > 1000 AND
+      items_sold > 500 AND
+      gmv_category IN ('beauty & personal care', 'health', 'sports & outdoor', 'food & beverage')
+ORDER BY gmv DESC;
+
+/* Joining 2 tables. 'tiktok' and 'numbers' to compare relations between both tables. Outputs creators
+with a follower count greater than 10,000 and over 500 prodcuts sold in order by products sold */
+SELECT tiktok.creator,
+	   tiktok.follower_count,
+       numbers.products_sold
+FROM tiktok
+JOIN numbers
+	ON tiktok.creator = numbers.creator
+WHERE follower_count > 10000 AND
+	  products_sold > 100
+ORDER BY numbers.products_sold DESC; 
+
+  
+SELECT COUNT(creator) AS num_users
+FROM tiktok
+WHERE follower_count > 10000;
